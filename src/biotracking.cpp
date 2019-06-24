88e8b20bb18cafb5f54638dc77f62595a6d19777
muritane <31107191+muritane@users.noreply.github.com>
2019-06-25 01:22:08 +0200

    Update biotracking.cpp

--- src/biotracking.cpp
+++ src/biotracking.cpp
@@ -12,6 +12,12 @@ Biotracking::Biotracking(ros::NodeHandle nh) : nh_(nh), tfListener(tfBuffer)
     
     pcl_cloud_publisher = nh_.advertise<PointCloud>("pcl_point_cloud", 10);
     pc2_subscriber = nh_.subscribe<sensor_msgs::PointCloud2>(topic_point_cloud, 1, &Biotracking::processPointCloud2, this);
+    
+    image_sub_ = it_.subscribe("/camera/image_raw", 1,
+      &ImageConverter::imageCb, this);
+    image_pub_ = it_.advertise("/image_converter/output_video", 1);
+
+    cv::namedWindow(OPENCV_WINDOW);
 }
 
 void Biotracking::processPointCloud2(const sensor_msgs::PointCloud2::ConstPtr& cloud_in)
@@ -30,8 +36,33 @@ void Biotracking::processPointCloud2(const sensor_msgs::PointCloud2::ConstPtr& c
     pass.filter(pass_through_filtered);
     
     pcl_cloud_publisher.publish(pass_through_filtered);
-    
-    
-    
-    
 }
+
+void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      // cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::CV_16UC1);
+      // image_encodings: rostopic echo /camera/depth/image --noarr
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_32FC1);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    // cv::normalize(cv_ptr->image, depthImg->image, 1, 0, cv::NORM_MINMAX);
+    
+    // Draw an example circle on the video stream
+    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+
+    // Update GUI Window
+    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    cv::waitKey(3);
+
+    // Output modified video stream
+    image_pub_.publish(cv_ptr->toImageMsg());
+  }
