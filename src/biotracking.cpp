8b557d393a80a2226ddb3c82b9d74466808cb3a3
Daniel Azanov <unduh@student.kit.edu>
2019-07-31 13:56:04 +0200

    back up commit

--- src/biotracking.cpp
+++ src/biotracking.cpp
@@ -2,6 +2,7 @@
 
 #include <biotracking/biotracking.h>
 #include <fstream>
+#include <iostream>
 
 
 int C = 640, R = 480; // x = c, y = r
@@ -14,6 +15,8 @@ Biotracking::Biotracking(ros::NodeHandle nh) : nh_(nh), tfListener(tfBuffer), it
     nh_.param("depth_image_topic", depth_image_topic, std::string("/camera/depth_registered/image_raw"));
     nh_.param("depth_image_pub", depth_image_pub, std::string("/biotracking/raw_image"));
     nh_.param("background_threshold", background_threshold, 3);
+    nh_.param("person_distance", person_distance, 2.);
+    nh_.param("person_distance", person_distance, 1.);
     
     nh_.param("lower_limit", lower_limit, 0.0);
     nh_.param("upper_limit", upper_limit, 1.0);
@@ -31,62 +34,25 @@ Biotracking::Biotracking(ros::NodeHandle nh) : nh_(nh), tfListener(tfBuffer), it
 	avg_image_pub_ = it_.advertise("/biotracking/avg_image", 1);
 	raw_image_8u_pub_ = it_.advertise("/biotracking/raw_image_8u", 1);
 	rgb_image_pub_ = it_.advertise("/biotracking/rgb_image_", 1);
+	mog2_pub_ = it_.advertise("/biotracking/mog2_image_", 1);
+    
+//     pMOG2 = bgsegm::createBackgroundSubtractorMOG(); //MOG2 approach
 
 //     cv::namedWindow(e);
 	
 // 	remainedImagesToCalcAvg = 100;
-	remainedImagesToCalcAvg = 1;
+// 	remainedImagesToCalcAvg = 1;
+	remainedImagesToCalcAvg = 10;
 	
 	avg_image.create(R,C,CV_32FC1);
     
     isCalculateAvgSrvCalled = isAvgCalculated = false;
+    
+    bottom_right_r = bottom_left_r = bottom_right_c = bottom_left_c = -1;
+    left_r = left_c = right_r = right_c = -1;
 }
 
-bool Biotracking::calculateAvgImage(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
-{
-	isCalculateAvgSrvCalled = true;
-    return true;
-}
 
-void Biotracking::processPointCloud2(const sensor_msgs::PointCloud2::ConstPtr& cloud_in)
-{
-    pcl::PCLPointCloud2::Ptr pcl_pc2 (new pcl::PCLPointCloud2());
-    pcl_conversions::toPCL(*cloud_in, *pcl_pc2);
-    PointCloud cloudXYZRGB;
-    pcl::fromPCLPointCloud2(*pcl_pc2, cloudXYZRGB);
-    
-    PointCloud pass_through_filtered;
-    pcl::PassThrough<Point> pass;
-    pass.setInputCloud(cloudXYZRGB.makeShared());
-    pass.setFilterFieldName("z");
-    pass.setFilterLimits(lower_limit, upper_limit);
-    pass.setFilterLimitsNegative (false);
-    pass.filter(pass_through_filtered);
-    
-    pcl_cloud_publisher.publish(pass_through_filtered);
-}
-
-void Biotracking::rgbImageCb(const sensor_msgs::ImageConstPtr& msg)
-{
-    if (!isCalculateAvgSrvCalled) { return; }
-    
-//     if (!left_r || !left_c || !right_r || !right_c) { return; }
-    
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
-    cv::circle(cv_ptr->image, cv::Point(left_r, left_c), 10, CV_RGB(255,0,0), CV_FILLED, 10,0);
-    cv::circle(cv_ptr->image, cv::Point(right_r, right_c), 10, CV_RGB(255,0,0), CV_FILLED, 10,0);
-    
-    rgb_image_pub_.publish(cv_ptr->toImageMsg());
-}
 
 void Biotracking::imageCb(const sensor_msgs::ImageConstPtr& msg)
 {
@@ -104,11 +70,18 @@ void Biotracking::imageCb(const sensor_msgs::ImageConstPtr& msg)
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
+    
+//     frame = cv_ptr->image;
+//     pMOG2->apply(cv_ptr->image, fgMaskMOG2);
 	
 	if (remainedImagesToCalcAvg > 0) 
 	{ 
 // 		avg_image += 0.01 * cv_ptr->image;
-		avg_image = cv_ptr->image;
+		avg_image = (1 / remainedImagesToCalcAvg) * cv_ptr->image;
+        
+        
+        
+        
 		remainedImagesToCalcAvg--;
 	}
 	
@@ -156,7 +129,9 @@ void Biotracking::imageCb(const sensor_msgs::ImageConstPtr& msg)
 //             }
 //             workingImg_ptr[c] = 255 * std::abs(avg_image_raw_ptr[c] - cv_ptr_image_ptr[c]);
             
-            if (diffMat_ptr[c] > 0.1 && cv_ptr_image_ptr[c] < 2. && cv_ptr_image_ptr[c] > 0.3)
+//             float diff = avg_image_raw_ptr[c] - cv_ptr_image_ptr[c];
+            
+            if (diffMat_ptr[c] > 0.1 && cv_ptr_image_ptr[c] < person_distance && cv_ptr_image_ptr[c] > 0.06)
             {
                 workingImg_ptr[c] = 255;
             } else {
@@ -164,11 +139,36 @@ void Biotracking::imageCb(const sensor_msgs::ImageConstPtr& msg)
             }
                 
             if (out == 1) {
-                s += "[" + std::to_string(cv_ptr_image_ptr[c]) + ", " + std::to_string(diffMat_ptr[c]) + "(" + std::to_string(r) + ", " + std::to_string(c) + ")]";
+                s += "[actual: " + std::to_string(cv_ptr_image_ptr[c]) + ", absdiff: " + std::to_string(diffMat_ptr[c]) + ", avg: " + std::to_string(avg_image_raw_ptr[c]) + "pos: (" + std::to_string(r) + ", " + std::to_string(c) + ")]";
 //                 s2 += "[" + std::to_string(raw_image_ptr[c]) + ", " + std::to_string(avg_image_ptr[c]) + "(" + std::to_string(r) + ", " + std::to_string(c) + ")]";
                 
             }
         }
+        
+//         int person_r, person_l;
+//         for (int c = 0; c <= workingImg.cols / 2; c++)
+//         {
+//             if (workingImg_ptr[c] > 0) 
+//             {
+//                 person_l = c;
+//                 break;
+//             }
+//         }
+//         
+//         for (int c = workingImg.cols - 1; c >= workingImg.cols / 2; c--)
+//         {
+//             if (workingImg_ptr[c] > 0) 
+//             {
+//                 person_r = c;
+//                 break;
+//             }
+//         }
+//         
+//         for (int c = person_l; c <= person_r; c++)
+//         {
+//             workingImg_ptr[c] = 255;
+//         }
+        
         if (out == 1) {
             s += "\n";
             
@@ -179,7 +179,7 @@ void Biotracking::imageCb(const sensor_msgs::ImageConstPtr& msg)
         std::ofstream ofstream("/home/student1/output.txt");
         ofstream << s;
         ofstream.close();
-        out = 0;
+//         out = 0;
     }
 //     cv::Mat workingImg;
 //     if (out == 1) {
@@ -212,14 +212,37 @@ void Biotracking::imageCb(const sensor_msgs::ImageConstPtr& msg)
 	erode(workingImg, erosion_dst, element);
 	cv::Mat subtract_dst = workingImg - erosion_dst;
 	
+    int bottom_r = subtract_dst.cols - 10;
+    bottom_right_r = bottom_left_r = bottom_r; 
+    uchar* bottom_ptr = subtract_dst.ptr<uchar>(bottom_r);
+    for (int c = subtract_dst.rows / 2; c < subtract_dst.rows; c++)
+    {
+        if (bottom_ptr[c] == 0) 
+        {
+            bottom_right_c = c;
+            break;
+        }
+    }
+    
+    for (int c = subtract_dst.rows / 2; c > 0; c--)
+    {
+        if (bottom_ptr[c] == 0) 
+        {
+            bottom_left_c = c;
+            break;
+        }
+    }
+    
+    
 	
 // 	left_r = left_c = right_r = right_c = -1;
-    for(int r = 0; r < subtract_dst.cols; r++) {
+    for(int r = 5; r < subtract_dst.cols / 2; r++) {
         // We obtain a pointer to the beginning of row r
         // cv::Vec3b* ptr = subtract_dst.ptr<cv::Vec3b>(r);
 		uchar* ptr = subtract_dst.ptr<uchar>(r);
 
 		int left, right;
+        left = right = -1;
         for(int c = 0; c < subtract_dst.rows; c++) {
             // We invert the blue and red values of the pixel
             // ptr[c] = cv::Vec3b(ptr[c][2], ptr[c][1], ptr[c][0]);
@@ -235,12 +258,16 @@ void Biotracking::imageCb(const sensor_msgs::ImageConstPtr& msg)
 				break;
 			}
 		}
-// 		if (right - left > subtract_dst.cols / 2) {
-        left_r = right_r = r;
-        left_c = left;
-        right_c = right;
-        break;
-// 		}
+		
+		if (left == -1 || right == -1) { continue; }
+		
+		if (right - left > bottom_factor * (bottom_right_c - bottom_left_c)) {
+            left_r = right_r = r;
+            left_c = left;
+            right_c = right;
+            
+            break;
+		}
     }
 // 	for(int r = 0; r < subtract_dst.rows; r++) {
 //         // We obtain a pointer to the beginning of row r
@@ -292,4 +319,72 @@ void Biotracking::imageCb(const sensor_msgs::ImageConstPtr& msg)
 	working_image_pub_.publish(msg_to_pub);
 	msg_to_pub = cv_bridge::CvImage(std_msgs::Header(), "8UC1", subtract_dst).toImageMsg();
 	subtract_image_pub_.publish(msg_to_pub);
+// 	msg_to_pub = cv_bridge::CvImage(std_msgs::Header(), "32FC1", fgMaskMOG2).toImageMsg();
+// 	mog2_pub_.publish(msg_to_pub);
+    
+    
+    int waitInt;
+//     std::cin >> waitInt;
+}
+
+
+
+
+bool Biotracking::calculateAvgImage(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
+{
+	isCalculateAvgSrvCalled = true;
+    return true;
+}
+
+
+
+void Biotracking::processPointCloud2(const sensor_msgs::PointCloud2::ConstPtr& cloud_in)
+{
+    pcl::PCLPointCloud2::Ptr pcl_pc2 (new pcl::PCLPointCloud2());
+    pcl_conversions::toPCL(*cloud_in, *pcl_pc2);
+    PointCloud cloudXYZRGB;
+    pcl::fromPCLPointCloud2(*pcl_pc2, cloudXYZRGB);
+    
+    PointCloud pass_through_filtered;
+    pcl::PassThrough<Point> pass;
+    pass.setInputCloud(cloudXYZRGB.makeShared());
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits(lower_limit, upper_limit);
+    pass.setFilterLimitsNegative (false);
+    pass.filter(pass_through_filtered);
+    
+    pcl_cloud_publisher.publish(pass_through_filtered);
+}
+
+
+void Biotracking::rgbImageCb(const sensor_msgs::ImageConstPtr& msg)
+{
+    if (!isCalculateAvgSrvCalled) { return; }
+    
+//     if (!left_r || !left_c || !right_r || !right_c) { return; }
+    
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+    
+    if (left_c != -1 && left_r != -1) 
+        cv::circle(cv_ptr->image, cv::Point(left_c, left_r), 10, CV_RGB(255,0,0), CV_FILLED, 10,0);
+    
+    if (right_c != -1 && right_r != -1) 
+        cv::circle(cv_ptr->image, cv::Point(right_c, right_r), 10, CV_RGB(255,0,0), CV_FILLED, 10,0);
+    
+    if (bottom_left_c != -1 && bottom_left_r != -1) 
+        cv::circle(cv_ptr->image, cv::Point(bottom_left_c, bottom_left_r), 10, CV_RGB(0,255,0), CV_FILLED, 10,0);
+    
+    if (bottom_right_c != -1 && bottom_right_r != -1) 
+        cv::circle(cv_ptr->image, cv::Point(bottom_right_c, bottom_right_r), 10, CV_RGB(0,255,0), CV_FILLED, 10,0);
+    
+    rgb_image_pub_.publish(cv_ptr->toImageMsg());
 }
